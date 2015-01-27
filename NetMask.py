2c03eb4a5c7cfaeb6b56a15dfa072f400f8730b9
Nick Day <git-hub@nickday.biz>
2015-01-27 22:00:35 +1100

    Convert NetMask to script

--- NetMask.py
+++ NetMask.py
@@ -11,6 +11,10 @@ def NetMask(mask_length):
 
     for octet in range(4):
         octets.append(str(int(bits[(octet * 8):((octet + 1) * 8)], base=2)))
-    print ".".join(octets)
+    return ".".join(octets)
     
     
+if __name__ == "__main__":
+    import sys
+    mask_length = int(sys.argv[1])
+    print NetMask(mask_length)
